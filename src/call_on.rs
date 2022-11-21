5adbce140b04111f56e23718ac1a79e327d2f6e8
Lukasz Szpakowski <luckboy@vp.pl>
2022-11-21 17:14:06 +0100

    Fixed return type.

--- src/call_on.rs
+++ src/call_on.rs
@@ -11,5 +11,5 @@ use crate::queue_context::*;
 
 pub trait CallOn: Send + Sync
 {
-    fn call_on(&mut self, client_context: &mut ClientContext, queue_context: &mut QueueContext, event: &Event) -> Option<EventOption>;
+    fn call_on(&mut self, client_context: &mut ClientContext, queue_context: &mut QueueContext, event: &Event) -> Option<Option<Event>>;
 }
