2e6a35c973f02aebfd70b8365a0bf71736f511c3
Lukasz Szpakowski <luckboy@vp.pl>
2023-01-12 17:37:47 +0100

    Fixed exit status of empty window example.

--- examples/empty_window.rs
+++ examples/empty_window.rs
@@ -5,6 +5,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
+use std::process::exit;
 use std::sync::Arc;
 use std::sync::RwLock;
 use lwltk::windows::ToplevelWindow;
@@ -40,9 +41,15 @@ fn main()
         Ok(mut app) => {
             match app.run() {
                 Ok(()) => (),
-                Err(err) => eprintln!("lwltk: {}", err),
+                Err(err) => {
+                    eprintln!("lwltk: {}", err);
+                    exit(1);
+                },
             }
         },
-        Err(err) => eprintln!("lwltk: {}", err),
+        Err(err) => {
+            eprintln!("lwltk: {}", err);
+            exit(1);
+        },
     }
 }
