b8367953d106cb911624ad7052930db6c0eb7f0d
Lukasz Szpakowski <luckboy@vp.pl>
2023-06-22 13:03:51 +0200

    Fixed.

--- src/widgets/grid_layout_widgets.rs
+++ src/widgets/grid_layout_widgets.rs
@@ -329,8 +329,12 @@ impl GridLayoutWidgets
         let y = orient_pos_y(pos, orient);
         let i = if start_y <= y && rem_y > y {
             Some((y - start_y / ((self.row_height + 1) as f64)).floor() as usize)
-        } else if rem_y <= y && end_y < y {
-            Some((((y - rem_y / (self.row_height as f64)).floor() as i32) + self.row_height_rem) as usize)
+        } else if rem_y <= y && end_y > y {
+            if self.row_height != 0 {
+                Some((((y - rem_y / (self.row_height as f64)).floor() as i32) + self.row_height_rem) as usize)
+            } else {
+                None
+            }
         } else {
             None
         };
