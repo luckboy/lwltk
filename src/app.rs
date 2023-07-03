87f5b04a5f966ebfb939df3202a9c9e19c2c74cb
Lukasz Szpakowski <luckboy@vp.pl>
2023-07-03 13:17:26 +0200

    Added documentation comments.

--- src/app.rs
+++ src/app.rs
@@ -17,6 +17,57 @@ use crate::theme::*;
 use crate::thread_signal::*;
 use crate::window_context::*;
 
+/// An application structure.
+///
+/// The application structure is a main structure that allows run an application. This structure
+/// contains contexts, the thread signal channel, and an application data.
+///
+/// # Examples
+/// ```no_run
+/// use lwltk::events::Event;
+/// use lwltk::events::EventOption;
+/// use lwltk::widgets::Button;
+/// use lwltk::windows::ToplevelWindow;
+/// use lwltk::AbsWidgetPath;
+/// use lwltk::App;
+/// use lwltk::HAlign;
+/// use lwltk::PreferredSize;
+/// use lwltk::Size;
+/// use lwltk::WindowIndex;
+/// use lwltk::WindowContext;
+/// use lwltk::VAlign;
+///
+/// struct AppData
+/// {
+///     window_index: WindowIndex,
+///     button_path: AbsWidgetPath,
+/// }
+///
+/// let mut app = App::new(|window_context, _, _, _| {
+///         let mut window = ToplevelWindow::new()?;
+///         window.set_title("Example");
+///         window.set_preferred_size(Size::new(Some(256), Some(256)));
+///         let window_idx = window_context.add_window(window)?;
+///         let mut button = Button::new("Button");
+///         button.set_h_align(HAlign::Center);
+///         button.set_v_align(VAlign::Center);
+///         let button_path = window_context.abs_widget_path1(window_idx, |window: &mut ToplevelWindow| window.set(button))?;
+///         Some(AppData {
+///                 window_index: window_idx,
+///                 button_path,
+///         })
+/// }, |window_context, app_data, _, _, _, _| {
+///         window_context.window_mut::<ToplevelWindow>(app_data.window_index)?.set_on(move |client_context, _, event| {
+///                 match event {
+///                     Event::Close => client_context.exit(),
+///                     _ => (),
+///                 }
+///                 Some(EventOption::Default)
+///         });
+///         Some(())
+/// }).unwrap();
+/// app.run().unwrap();
+/// ```
 pub struct App<T>
 {
     client_display: ClientDisplay,
@@ -30,16 +81,39 @@ pub struct App<T>
 
 impl<T> App<T>
 {
+    /// Creates an application object.
+    ///
+    /// This method takes two arguments which are functions. The first function creates an
+    /// application data. The second functions sets the application data, widgets, and windows. The
+    /// first function that takes arguments:
+    /// - the reference to the window context
+    /// - the reference-counting pointer to the window context
+    /// - the reference-counting pointer to the queue context
+    /// - the thread signal sender
+    ///
+    /// The second function that takes arguments:
+    /// - the reference to the window context
+    /// - the reference to the application data
+    /// - the reference-counting pointer to the window context
+    /// - the reference-counting pointer to the queue context
+    /// - the thread signal sender
+    /// - the reference-counting pointer to the application data
     pub fn new<F, G>(creating_f: F, setting_f: G) -> Result<Self, ClientError>
         where F: FnOnce(&mut WindowContext, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender) -> Option<T>,
               G: FnOnce(&mut WindowContext, &mut T, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender, Arc<RwLock<T>>) -> Option<()>
     { Self::new_with_dyn_theme(theme_from_env()?, creating_f, setting_f) }
 
+    /// Creates an application object with a theme.
+    ///
+    /// See [new](Self::new) for information about two last arguments.
     pub fn new_with_theme<U: Theme + 'static, F, G>(theme: U, creating_f: F, setting_f: G) -> Result<Self, ClientError>
         where F: FnOnce(&mut WindowContext, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender) -> Option<T>,
               G: FnOnce(&mut WindowContext, &mut T, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender, Arc<RwLock<T>>) -> Option<()>
     { Self::new_with_dyn_theme(Box::new(theme), creating_f, setting_f) }
     
+    /// Creates an application object with a dynamic theme.
+    ///
+    /// See [new](Self::new) for information about two last arguments.
     pub fn new_with_dyn_theme<F, G>(theme: Box<dyn Theme>, creating_f: F, setting_f: G) -> Result<Self, ClientError>
         where F: FnOnce(&mut WindowContext, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender) -> Option<T>,
               G: FnOnce(&mut WindowContext, &mut T, Arc<RwLock<WindowContext>>, Arc<Mutex<QueueContext>>, ThreadSignalSender, Arc<RwLock<T>>) -> Option<()>
@@ -102,21 +176,29 @@ impl<T> App<T>
         res2
     }
 
+    /// Returns the reference-counting pointer to the client context.
     pub fn client_context(&self) -> Rc<RefCell<ClientContext>>
     { self.client_context.clone() }
 
+    /// Returns the reference-counting pointer to the window context.
     pub fn window_context(&self) -> Arc<RwLock<WindowContext>>
     { self.window_context.clone() }
 
+    /// Returns the reference-counting pointer to the queue context.
     pub fn queue_context(&self) -> Arc<Mutex<QueueContext>>
     { self.queue_context.clone() }
 
+    /// Returns the thread signal sender.
     pub fn thread_signal_sender(&self) -> ThreadSignalSender
     { self.thread_signal_sender }
     
+    /// Returns the reference-counting pointer to the application data.
     pub fn data(&self) -> Arc<RwLock<T>>
     { self.data.clone() }
     
+    /// Runs the application.
+    ///
+    /// This method executes a main loop for a graphic thread.
     pub fn run(&mut self) -> Result<(), ClientError>
     { run_main_loop(&mut self.client_display, self.client_context.clone(), self.window_context.clone(), self.queue_context.clone(), self.thread_signal_sender, self.thread_signal_receiver) }
 }
--- src/window_context.rs
+++ src/window_context.rs
@@ -13,6 +13,11 @@ use crate::widget::*;
 use crate::window::*;
 use crate::window_container::*;
 
+/// A structure of window context.
+///
+/// This structure is used to menage widgets and windows. The structure of window context contains 
+/// a theme and a window container. The window container contains windows. See [WindowContainer] for
+/// more informations.
 pub struct WindowContext
 {
     pub(crate) theme: Box<dyn Theme>,
@@ -37,27 +42,35 @@ impl WindowContext
         }
     }
     
+    /// Returns the reference to the theme.
     pub fn theme(&self) -> &dyn Theme
     { &*self.theme }
     
+    /// Returns the reference to the window container.
     pub fn window_container(&self) -> &WindowContainer
     { &self.window_container }
 
+    /// Returns the mutable reference to the window container.
     pub fn window_container_mut(&mut self) -> &mut WindowContainer
     { &mut self.window_container }
     
+    /// Returns the current window index.
     pub fn current_window_index(&self) -> Option<WindowIndex>
     { self.current_window_index }
 
+    /// Returns the current position.
     pub fn current_pos(&self) -> Option<Pos<f64>>
     { self.current_pos }
     
+    /// Returns focused window index.
     pub fn focused_window_index(&self) -> Option<WindowIndex>
     { self.focused_window_index }
     
+    /// Sets the focused window index.
     pub fn set_focused_window_index(&mut self, idx: Option<WindowIndex>)
     { self.focused_window_index = idx; }
     
+    /// Returns the reference to the dynamic current window.
     pub fn dyn_current_window(&self) -> Option<&dyn Window>
     {
         match self.current_window_index {
@@ -66,6 +79,7 @@ impl WindowContext
         }
     }
 
+    /// Returns the mutable reference to the dynamic current window.
     pub fn dyn_current_window_mut(&mut self) -> Option<&mut dyn Window>
     {
         match self.current_window_index {
@@ -74,6 +88,7 @@ impl WindowContext
         }
     }
 
+    /// Returns the reference to the current window.
     pub fn current_window<T: Any>(&self) -> Option<&T>
     {
         match self.current_window_index {
@@ -82,6 +97,7 @@ impl WindowContext
         }
     }
 
+    /// Returns the mutable reference to the current window.
     pub fn current_window_mut<T: Any>(&mut self) -> Option<&mut T>
     {
         match self.current_window_index {
@@ -90,50 +106,65 @@ impl WindowContext
         }
     }
 
+    /// See [WindowContainer::add_dyn].
     pub fn add_dyn_window(&mut self, window: Box<dyn Window>) -> Option<WindowIndex>
     { self.window_container.add_dyn(window) }
     
+    /// See [WindowContainer::add].
     pub fn add_window<T: Window + 'static>(&mut self, window: T) -> Option<WindowIndex>
     { self.window_container.add_dyn(Box::new(window)) }
     
+    /// See [WindowContainer::remove].
     pub fn remove_window(&mut self, idx: WindowIndex) -> Option<Box<dyn Window>>
     { self.window_container.remove(idx) }
 
+    /// See [WindowContainer::dyn_window].
     pub fn dyn_window(&self, idx: WindowIndex) -> Option<&dyn Window>
     { self.window_container.dyn_window(idx) }
 
+    /// See [WindowContainer::dyn_window_mut].
     pub fn dyn_window_mut(&mut self, idx: WindowIndex) -> Option<&mut dyn Window>
     { self.window_container.dyn_window_mut(idx) }
 
+    /// See [WindowContainer::window].
     pub fn window<T: Any>(&self, idx: WindowIndex) -> Option<&T>
     { self.window_container.window(idx) }
 
+    /// See [WindowContainer::window_mut].
     pub fn window_mut<T: Any>(&mut self, idx: WindowIndex) -> Option<&mut T>
     { self.window_container.window_mut(idx) }
 
+    /// See [WindowContainer::abs_widget_path1].
     pub fn abs_widget_path1<C: Container + Any, F>(&mut self, idx: WindowIndex, f: F) -> Option<AbsWidgetPath>
         where F: FnOnce(&mut C) -> Option<WidgetIndexPair>
     { self.window_container.abs_widget_path1(idx, f) }
     
+    /// See [WindowContainer::dyn_widget].
     pub fn dyn_widget(&self, path: &AbsWidgetPath) -> Option<&dyn Widget>
     { self.window_container.dyn_widget(path) }
 
+    /// See [WindowContainer::dyn_widget_mut].
     pub fn dyn_widget_mut(&mut self, path: &AbsWidgetPath) -> Option<&mut dyn Widget>
     { self.window_container.dyn_widget_mut(path) }
 
+    /// See [WindowContainer::widget].
     pub fn widget<T: Any>(&self, path: &AbsWidgetPath) -> Option<&T>
     { self.window_container.widget(path) }
 
+    /// See [WindowContainer::widget_mut].
     pub fn widget_mut<T: Any>(&mut self, path: &AbsWidgetPath) -> Option<&mut T>
     { self.window_container.widget_mut(path) }
     
+    /// See [WindowContainer::abs_widget_path].
     pub fn abs_widget_path<T: Any, F>(&mut self, path: &AbsWidgetPath, f: F) -> Option<AbsWidgetPath>
         where F: FnOnce(&mut T) -> Option<WidgetIndexPair>
     { self.window_container.abs_widget_path(path, f) }
     
+    /// See [WindowContainer::set_parent].
     pub fn set_parent_window(&mut self, child_idx: WindowIndex, parent_idx: WindowIndex, pos: Pos<i32>) -> Option<()>
     { self.window_container.set_parent(child_idx, parent_idx, pos) }
 
+    /// See [WindowContainer::unset_parent].
     pub fn unset_parent_window(&mut self, child_idx: WindowIndex) -> Option<()>
     { self.window_container.unset_parent(child_idx) }
 }
